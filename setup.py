18e935ecf7ebe564f4ff7643326deb770ee972f2
AintSmurf <monkeydyasuo1@gmail.com>
2023-10-25 22:06:29 +0300

    added file that updates packages

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
-setup(name="Ecommerce Site Testing",
-    version="2.8",
+setup(
+    name="Ecommerce Site Testing",
+    version="2.9",
     description="WordPress Testing",
     packages=find_packages(),
     install_requires=[
-        "PyMySQL==1.0.2",
-        "pyparsing==3.0.9",
-        "pytest==7.1.2",
-        "pytest-html == 3.1.1",
-        "requests==2.28.1",
+        "PyMySQL==1.1.0",
+        "pyparsing==3.1.1",
+        "pytest==7.4.3",
+        "pytest-html==4.0.2",
+        "requests==2.31.0",
         "requests-oauthlib==1.3.1",
-        "selenium==4.5.0",
-        "WooCommerce==3.0.0",]
-      )
+        "selenium==4.14.0",
+        "WooCommerce==3.0.0",
+        "allure-pytest==2.13.2",
+        "beautifulsoup4==4.9.3"
+    ]
+)
