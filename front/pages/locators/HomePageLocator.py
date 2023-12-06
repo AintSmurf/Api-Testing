c830bf69b5e99be7a3d28e842effeb9db1a2c50b
AintSmurf <monkeydyasuo1@gmail.com>
2023-12-06 21:47:32 +0200

    added script to geneate allure report,updated chrome driver.

--- front/pages/locators/HomePageLocator.py
+++ front/pages/locators/HomePageLocator.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class HomePageLocator():
-    ADD_TO_CART_BUTTON =(By.CSS_SELECTOR, 'a.add_to_cart_button')
-    CART_BUTTON =(By.XPATH, '//*[@id="modal-2-content"]/ul/li[1]/a')
+    ADD_TO_CART_BUTTON =(By.XPATH, '//a[@data-product_sku="woo-beanie"]')
+    CART_BUTTON =(By.XPATH, '//a[@href="http://localhost:8888/TestSite/cart/"]')
     VERIFCATION_TEXT =(By.CSS_SELECTOR, 'added_to_cart')
 
